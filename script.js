513743eaaf6ced5f528b817604315c89068cdeae
vetex007 <124381334+vetex007@users.noreply.github.com>
2026-02-06 16:27:52 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function showRose() {
+  document.getElementById("roseSection").style.display = "block";
+}
+
+function showPhotos() {
+  document.getElementById("photos").style.display = "block";
+}
